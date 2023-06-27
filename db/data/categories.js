18eb9e3aba1016025e346fe2aaf47657dce1814b
bainesface <bainesface@gmail.com>
2023-06-27 10:15:14 +0100

    create initial data set

---
+++ db/data/categories.js
@@ -0,0 +1,20 @@
+export const categories = [
+  {
+    category_name: 'Biscuit',
+  },
+  {
+    category_name: 'Fruit',
+  },
+  {
+    category_name: 'Chocolate',
+  },
+  {
+    category_name: 'Pastry',
+  },
+  {
+    category_name: 'Crisps',
+  },
+  {
+    category_name: 'Drink',
+  },
+];
---
+++ db/data/snacks.js
@@ -0,0 +1,31 @@
+export const snacks = [
+  {
+    snack_name: 'Kit Kat',
+    flavour_text: 'time for a break',
+  },
+  {
+    snack_name: 'Hobnob',
+    flavour_text: 'a modern classic',
+  },
+  {
+    snack_name: 'Banana',
+    flavour_text: 'humans open them upside down',
+  },
+  {
+    snack_name: 'Coffee',
+    flavour_text: 'the only thing we truly need',
+  },
+  {
+    snack_name: 'Pringles',
+    flavour_text: "once you pop you can't stop",
+  },
+  {
+    snack_name: 'Butter Croissant',
+    flavour_text: 'always appreciated',
+  },
+  {
+    snack_name: 'Revels',
+    flavour_text:
+      "actually belong to Sam and he doesn't know I'm stealing them, shhhhhh",
+  },
+];
